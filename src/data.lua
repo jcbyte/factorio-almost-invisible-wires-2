eefc4458704b1c6a6549d6980acc92f857406727
Joel Cutler <joelcutler108@gmail.com>
2025-07-29 17:58:11 +0100

    fork existing mod https://mods.factorio.com/mod/AlmostInvisibleElectricWires

---
+++ src/data.lua
@@ -0,0 +1,297 @@
+	if settings.startup["wire-color"].value == "Invisible" then
+	data.raw["utility-sprites"]["default"].
+    copper_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/copper-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+      height = 46,
+	  flags = { "no-crop" },
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/hr-copper-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+     }
+	
+    elseif settings.startup["wire-color"].value == "Visible 80%" then
+    data.raw["utility-sprites"]["default"].
+    copper_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/80-copper-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+      height = 46,
+	  flags = { "no-crop" },
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/80-hr-copper-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+     }
+	 
+	 
+    elseif settings.startup["wire-color"].value == "Visible 50%" then
+    data.raw["utility-sprites"]["default"].
+    copper_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/50-copper-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/50-hr-copper-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+     }
+	 
+	 elseif settings.startup["wire-color"].value == "Visible 30%" then
+    data.raw["utility-sprites"]["default"].
+    copper_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/30-copper-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/30-hr-copper-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+     }
+	end
+	
+	
+	
+	
+	if settings.startup["wire-color-g"].value == "Invisible" then
+	data.raw["utility-sprites"]["default"].
+    green_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/green-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/hr-green-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	green_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+	  flags = { "no-crop" },
+      height = 46,
+     }
+	
+    elseif settings.startup["wire-color-g"].value == "Blue" then
+    
+    
+data.raw["item"]["green-wire"].icon_size = 32
+
+
+data.raw["utility-sprites"]["default"].
+    green_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/green-wire-b.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+      ---[[ hr_version = {
+  --      filename = "__AlmostInvisibleElectricWires__/graphics/green-wire-b.png",
+   --     priority = "extra-high-no-scale",
+    --    flags = { "no-crop" },
+    --    width = 448,
+    --    height = 92,
+    --    scale = 0.5
+   --   } 
+    }
+	data.raw["item"]["green-wire"].icon = "__AlmostInvisibleElectricWires__/graphics/green-wire-icon.png"
+	data.raw["utility-sprites"]["default"].
+	green_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+    }
+	
+	elseif settings.startup["wire-color-g"].value == "Visible 50%" then
+    data.raw["utility-sprites"]["default"].
+    red_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/50-green-wire.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+      height = 46,
+	  flags = { "no-crop" },
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/50-hr-green-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	red_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+    }
+	
+	end
+	
+	
+	if settings.startup["wire-color-r"].value == "Invisible" then
+	data.raw["utility-sprites"]["default"].
+    red_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/red-wire.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/hr-red-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	red_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+     }
+	
+    elseif settings.startup["wire-color-r"].value == "Yellow" then
+    data.raw["item"]["red-wire"].icon_size = 32
+    data.raw["utility-sprites"]["default"].
+    red_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/red-wire-y.png",
+      priority = "extra-high-no-scale",
+      width = 224,
+      height = 46,
+	  flags = { "no-crop" },
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/hr-red-wire-y.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["item"]["red-wire"].icon = "__AlmostInvisibleElectricWires__/graphics/red-wire-icon.png"
+	data.raw["utility-sprites"]["default"].
+	red_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+    }
+	
+	elseif settings.startup["wire-color-r"].value == "Visible 50%" then
+    data.raw["utility-sprites"]["default"].
+    red_wire =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/50-red-wire.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+      hr_version = {
+        filename = "__AlmostInvisibleElectricWires__/graphics/50-hr-red-wire.png",
+        priority = "extra-high-no-scale",
+        flags = { "no-crop" },
+        width = 448,
+        height = 92,
+        scale = 0.5
+      }
+    }
+	data.raw["utility-sprites"]["default"].
+	red_wire_shadow =
+    {
+      filename = "__AlmostInvisibleElectricWires__/graphics/transparent.png",
+      priority = "extra-high-no-scale",
+	  flags = { "no-crop" },
+      width = 224,
+      height = 46,
+    }
+	
+	end
---
+++ src/settings.lua
@@ -0,0 +1,34 @@
+data:extend(
+{
+{
+type = "string-setting",
+name = "wire-color",
+setting_type = "startup",
+default_value = "Visible 30%",
+allowed_values = {"Invisible", "Visible 30%", "Visible 50%", "Visible 80%", "Default"},
+localised_name = "Copper Wire Color"
+},
+
+
+
+{
+type = "string-setting",
+name = "wire-color-g",
+setting_type = "startup",
+default_value = "Visible 50%",
+allowed_values = {"Green", "Visible 50%","Blue","Invisible"},
+localised_name = "Green Wire Color"
+},
+
+{
+type = "string-setting",
+name = "wire-color-r",
+setting_type = "startup",
+default_value = "Visible 50%",
+allowed_values = {"Red", "Visible 50%","Yellow","Invisible"},
+localised_name = "Red Wire Color"
+},
+
+
+
+})
